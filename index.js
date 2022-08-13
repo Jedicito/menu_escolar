1705d291c4877a3fe52f6d9770bd2dd8ed46d3f5
Jedicillo <seron.cesar@gmail.com>
2022-08-13 00:12:27 -0400

    Haciendo el login

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ require('dotenv').config();
 app.listen(3000, () => console.log("Servidor levantado en htttp://localhost:3000"));
 
 app.use(express.json());
+app.use("/bootstrap", express.static(`${__dirname}/node_modules/bootstrap/dist`));
 
 app.get("/", (req, res) => {
+    res.json();
+});
 
+app.get("/login", async (req, res) => {
+    res.sendFile(`${__dirname}/views/login.html`);
 });
