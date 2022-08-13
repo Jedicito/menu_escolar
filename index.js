3509346ce5cf95cccf056154244651fff2ced507
Jedicillo <seron.cesar@gmail.com>
2022-08-13 01:17:09 -0400

    Trabajando en Inicio

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
+const cp = require('cookie-parser');
+const { v4:uuidv4 } = require('uuid');
+const jwt = require('jsonwebtoken');
 
 app.listen(3000, () => console.log("Servidor levantado en htttp://localhost:3000"));
 
@@ -8,7 +11,13 @@ app.use(express.json());
 app.use("/bootstrap", express.static(`${__dirname}/node_modules/bootstrap/dist`));
 
 app.get("/", (req, res) => {
-    res.json();
+    const autenticacion = req.cookies['menu_aut']
+    if (condition) {
+        
+    } else {
+        
+    }
+
 });
 
 app.get("/login", async (req, res) => {
